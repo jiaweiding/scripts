63825d4e2b9c74375978c0f38bce37617d97751e
jiaweiding <iewaijgnid@gmail.com>
2025-03-13 16:03:17 +0800

    增加小红书

--- Scripts/12306_ads.js
+++ Scripts/12306_ads.js
@@ -1,1 +1,23 @@
-!function(){let e,a=JSON.parse($request.body);e="0007"===a.placementNo?'{"materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}':"G0054"===a.placementNo?'{"code":"00","materialsList":[{}]}':'{"code":"00","message":"无广告返回"}';"undefined"!=typeof $task?$done({body:e}):$done({response:{body:e}})}();
+/*
+引用地址https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/12306.js
+*/
+// 2024-02-19 10:45
+
+let body = "";
+let obj = JSON.parse($request.body);
+const isQuanX = typeof $task !== "undefined";
+
+if (obj.placementNo === "0007") {
+  body =
+    '{"code":"00","materialsList":[{"billMaterialsId":"255","filePath":"h","creativeType":1}],"advertParam":{"skipTime":1}}';
+} else if (obj.placementNo === "G0054") {
+  body = '{"code":"00","materialsList":[]}';
+} else {
+  body = '{"code":"00","message":"无广告返回"}';
+}
+
+if (isQuanX) {
+  $done({ body });
+} else {
+  $done({ response: { body } });
+}
